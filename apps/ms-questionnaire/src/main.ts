a86dea1d0ed042a9364670e0fe2bfe0bcac70ec9
pwn922 <mauricio41533@gmail.com>
2024-11-20 20:40:48 -0800

    feat: increment limit

--- apps/ms-questionnaire/src/main.ts
+++ apps/ms-questionnaire/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { MsQuestionnaireModule } from './ms-questionnaire.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(MsQuestionnaireModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(3002);
 }
 bootstrap();
